b0ebed99ed9fa013e7f5fbbd0b6acce496e5be7a
Jakob Zornig Kjellberg <jakob.zornigkjellberg@gmail.com>
2024-12-04 12:26:14 +0100

    Working on getting better at rust

--- main.rs
+++ main.rs
@@ -1,3 +1,9 @@
+use std::io;
 fn main() {
-    println!("Hello world);
+    println!("Guess a number");
+    let mut guess = String::new();
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+    println!("You guessed the number {}",guess);
 }
